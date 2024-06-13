308b706e1538eb67a6db1bb5cf1641c7c6cc2504
wyllbrayner <wyllbrayner@yahoo.com.br>
2024-06-13 10:26:31 -0300

    Challenge Alura encrypt text

---
+++ script.js
@@ -0,0 +1,92 @@
+function getSelectorFromTag(tag) {
+    return (document.querySelector(tag));
+}
+
+function insertTextIntoTag(tag, text) {
+    document.getElementById(tag).innerHTML = text;
+}
+
+function startFields() {
+    insertTextIntoTag('none', '');
+    document.querySelector("#txt-area1").value = "";
+}
+
+startFields();
+
+function encodeText (text, seq) {
+    var text1;
+
+    text = text.replace(/e/igm, "enter");
+    text = text.replace(/i/igm, "imes");
+    text = text.replace(/a/igm, "ai");
+    text = text.replace(/o/igm, "ober");
+    text = text.replace(/u/igm, "ufat");
+    if (seq != 10) {
+        console.log(seq);
+        text1 = encodeText(text, ++seq);
+        return (text1);
+    }
+    return (text);
+}
+
+function decodeText(text, seq) {
+    var text1;
+
+    text = text.replace(/enter/igm, 'e');
+    text = text.replace(/imes/igm, 'i');
+    text = text.replace(/ai/igm, 'a');
+    text = text.replace(/ober/igm, 'o');
+    text = text.replace(/ufat/igm, 'u');
+    if (seq != 10) {
+        console.log(seq);
+        text1 = decodeText(text, ++seq);
+        return (text1);
+    }
+    return (text);
+}
+
+function actualizeTxtArea2 (text) {
+    document.querySelector('img.icone').style.display = "none";
+    insertTextIntoTag('none', "");
+    insertTextIntoTag('txt-area2', text);
+    insertTextIntoTag('copy', 
+        '<button onclick="copy()" class="btn-3">Copiar</button>');
+}
+
+function encrypt() {
+    var textValue;
+
+    textValue = getSelectorFromTag('#txt-area1');
+    if (textValue.value.length == 0) {
+        insertTextIntoTag('none', 
+            "<h2 id='none'>Nenhuma mensagem encontrada</h2>");
+    } else {
+        var textEncypted;
+
+        textEncypted = encodeText(textValue.value, 0);
+        actualizeTxtArea2(textEncypted);
+    }
+}
+
+function decrypt() {
+    var textValue;
+
+    textValue = getSelectorFromTag('#txt-area1');
+    if (textValue.value.length == 0) {
+        insertTextIntoTag('none', 
+            "<h2 id='none'>Nenhuma mensagem encontrada</h2>");
+        document.querySelector('img.icone').style.display;
+    } else {
+        var textDecrypted;
+
+        textDecrypted = decodeText(textValue.value, 0);
+        actualizeTxtArea2(textDecrypted);
+    }
+}
+
+function copy() {
+    document.querySelector('#txt-area2').select();
+    document.execCommand('copy');
+    insertTextIntoTag("msg", "Texto copiado para a área de transferência!");
+    document.querySelector("#txt-area1").value = "";
+}
